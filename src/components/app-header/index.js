bb27335de370deea29283e1ab94c87831f35c47e
DESKTOP-48043F5\60375 <yuanasdfgh@gmail.com>
2021-04-26 00:41:40 +0800

    header

--- src/components/app-header/index.js
+++ src/components/app-header/index.js
@@ -1,9 +1,21 @@
 import React, { memo } from "react";
 
+import { NavLink } from "react-router-dom";
+import { HeaderWrapper, HeaderLeft, HeaderRight } from "./style";
+
 export default memo(function AppHeader() {
   return (
-    <div>
-      <h2> AppHearder </h2>
-    </div>
+    <HeaderWrapper>
+      <div className="content wrap-v1">
+        <HeaderLeft>
+          <a href="https://www.baudu.com" className="logo sprite_01"></a>
+        </HeaderLeft>
+        <HeaderRight>right</HeaderRight>
+        <NavLink to="/">发现音乐</NavLink>
+        <NavLink to="/friend">我的朋友</NavLink>
+        <NavLink to="/mine">我的音乐</NavLink>
+      </div>
+      <div className="divider"></div>
+    </HeaderWrapper>
   );
 });
---
+++ src/components/app-header/style.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const HeaderWrapper = styled.div`
+  height: 75px;
+  background: #242424;
+  font-size: 18px;
+  color: #fff;
+  .content {
+    height: 70px;
+    display: flex;
+    justify-content: space-between;
+  }
+  .divider {
+    height: 5px;
+    background-color: #c20c0c;
+  }
+`;
+export const HeaderLeft = styled.div`
+  .logo {
+    display: block;
+    width: 176px;
+    height: 69px;
+    background-position: 0 0;
+  }
+`;
+export const HeaderRight = styled.div``;
